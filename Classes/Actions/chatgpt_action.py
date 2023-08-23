d4ca3851d5a26c94bf91962e29e08aa4af2eaf52
Gabriel Agrela <100cabessa@gmail.com>
2023-08-23 22:10:09 +0100

    lyceum rework now it uses a database with all the answers, in case there is a new question, he uses the old chatgpt method.

--- Classes/Actions/chatgpt_action.py
+++ Classes/Actions/chatgpt_action.py
@@ -7,15 +7,15 @@ from Actions.manual_click_action import ManualClickAction
 import time
 from termcolor import colored
 import os
+from global_vars import GlobalVars
+
 
 class ChatGPTAction(Action):
-    def __init__(self,midterm=False, prefix= "", filepath="string.txt", delay=0, retard =0):
+    def __init__(self,midterm=False, filepath="string.txt", delay=0, retard =0):
         
         load_dotenv()
         openai.api_key = os.getenv('OPENAI_KEY')
         self.message = ""
-        self.prefix = prefix
-        self.filepath = filepath
         self.midterm = midterm
         self.delay = delay
         self.retard = retard
@@ -23,46 +23,25 @@ class ChatGPTAction(Action):
         self.functions = [
         {
             "name": "return_option_based_on_prompt",
-            "description": "Returns the response to the question in the prompt, thinking step by step, ignoring the answer options, returns the chosen answer option (A, B, C or D) based on the prompt, and returns the percentage of how certain it is of that answer option, will return 0 if unsure.",
+            "description": "thinking step by step, ignoring the answer options, returns the chosen answer option (A, B, C or D) based on the prompt",
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "answerComplete": {
-                        "type": "string",
-                        "description": "very short answer to the question in the prompt.",
-                    },
                     "answer": {
                         "type": "string",
                         "enum": ["A", "B", "C", "D"],
                         "description": "The chosen answer option to the question in the prompt.",
                     },
-                    
-                    "certainty": {
-                        "type": "integer",
-                        "description": "0 if it is unsure of the response, 100 if it is sure of the response.",
-                    },
                 },
-                "required": ["answer,certainty,answerComplete"],
+                "required": ["answer"],
             },
         }
         ]
-
-    def find_option(file_path):
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-
-        for line in lines:
-            if line.startswith((' A:', ' B:', ' C:', ' D:')):
-                return line.strip()  # Removing trailing new line
                     
     def execute(self):
-        os.system('cls')
-        with open(self.filepath, 'r') as file:
-            self.message = file.read()
-            print(f"User: {self.message}")
-        #concatenate prefix and message
-        self.message = self.prefix + self.message
-        
+        #os.system('cls')
+        self.message = "Question:" +  GlobalVars().Q + "\n" + "A:" + GlobalVars().A + "\n" + "B:" + GlobalVars().B + "\n" + "C:" + GlobalVars().C + "\n" + "D:" + GlobalVars().D
+        print("\n\n")
         self.messages.append({"role": "user", "content": (self.message)},)
         chat = openai.ChatCompletion.create(
             model="gpt-4-0613",
@@ -79,66 +58,33 @@ class ChatGPTAction(Action):
             function_arguments = json.loads(response_message["function_call"]["arguments"])
             function_response = function_arguments["answer"]
 
-            
-
-            with open('string.txt', 'r') as file:
-                # Read all lines in the file
-                lines = file.readlines()
-
-                # Initialize tempOption as None (it will remain None if no matching line is found)
-                tempOption = None
-
-                # Loop over all lines
-                for line in lines:
-                    # Check if the line starts with 'function_response'
-                    if line.startswith(function_response+":"):
-                        # Extract the option from the line (assuming it's the next character)
-                        tempOption = line
-                        break
-
-                            
-
-            try:
-                print("\n\n I think it's " , colored(tempOption.replace("\n",""),"red"), " with " , colored(function_arguments["certainty"],"green"), "%"," certainty because: \n", function_arguments["answerComplete"])
-            except:
-                try:
-                    print("\n\n I think it's " , colored(function_response,"red"), " with " , colored(function_arguments["certainty"],"green"), "%"," certainty because: \n", function_arguments["answerComplete"])
-                except:
-                    print("fucked up")
-            
+            print("\n\n I think it's " , colored(function_response,"red"))
 
             self.messages.clear()
             self.messages = [{"role": "system", "content": "You are a quizz assistant in the game Rise of Kingdoms."}]
 
             # Switch case for reply A, B, C, D, or E
             if not self.midterm:
-                # make function_arguments["certainty"] to int and see if its < 95
-                if function_arguments["certainty"] >= 95:
-                    if function_response == "E":
-                        print("Not Sure")
-                    elif function_response == "A":
-                        ManualClickAction(40,48).execute()
-                    elif function_response == "B":
-                        ManualClickAction(60,50).execute()
-                    elif function_response == "C":
-                       ManualClickAction(40,58).execute()
-                    elif function_response == "D":
-                        ManualClickAction(60,58).execute()
+                if function_response == "A":
+                    ManualClickAction(40,48).execute()
+                elif function_response == "B":
+                    ManualClickAction(60,50).execute()
+                elif function_response == "C":
+                    ManualClickAction(40,58).execute()
+                elif function_response == "D":
+                    ManualClickAction(60,58).execute()
                 else:
                     print("")
                 
             else:
-                if function_arguments["certainty"] >= 95:
-                    if function_response == "E":
-                        print("Not Sure")
-                    elif function_response == "A":
-                        ManualClickAction(37,55).execute()
-                    elif function_response == "B":
-                        ManualClickAction(60,55).execute()
-                    elif function_response == "C":
-                        ManualClickAction(37,63).execute()
-                    elif function_response == "D":
-                        ManualClickAction(60,63).execute()
+                if function_response == "A":
+                    ManualClickAction(37,55).execute()
+                elif function_response == "B":
+                    ManualClickAction(60,55).execute()
+                elif function_response == "C":
+                    ManualClickAction(37,63).execute()
+                elif function_response == "D":
+                    ManualClickAction(60,63).execute()
                 
 
         
